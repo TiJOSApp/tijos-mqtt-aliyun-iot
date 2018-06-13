410922619c840b87e450af1e0b0eb4d0c7c09106
lemon <lemon.yang@tijos.net>
2018-06-13 11:53:52 +0800

    Update samples and documents

--- TiJOSApp/Application/src/net/tijos/aliyun/Main.java
+++ TiJOSApp/Application/src/net/tijos/aliyun/Main.java
@@ -3,29 +3,31 @@ package net.tijos.aliyun;
 import java.io.IOException;
 import java.util.Formatter;
 
-import tijos.framework.networkcenter.TiDNS;
-import tijos.framework.networkcenter.TiWLAN;
+import tijos.framework.networkcenter.dns.TiDNS;
+import tijos.framework.platform.wlan.TiWiFi;
 import tijos.framework.networkcenter.mqtt.MqttClient;
 import tijos.framework.networkcenter.mqtt.MqttClientListener;
 import tijos.framework.networkcenter.mqtt.MqttConnectOptions;
 
+//"product_key":"b1ijiAZqEdV","device_name":"tikit-demo","device_secret":"iEANyI5Id9alYDNe2K6dBD6omsGWWKLY",
 
 public class Main {
 	
 	private static final long timestamp = System.currentTimeMillis();
-	private static final String PRODUCT_KEY = "IK5KVAPpndB";
-	private static final String DEVICE_NAME = "ESP8266-000001";//System.getProperty("host.name");
+	private static final String PRODUCT_KEY = "b1ijiAZqEdV";
+	private static final String DEVICE_NAME = "tikit-demo";//System.getProperty("host.name");
 	private static final String CLIENT_ID = format("%s|securemode=3,signmethod=hmacsha1,timestamp=%d|", DEVICE_NAME, timestamp);
 	private static final String BROKER = format("tcp://%s.iot-as-mqtt.cn-shanghai.aliyuncs.com:1883", PRODUCT_KEY);
 	private static final String USER_NAME = format("%s&%s", DEVICE_NAME, PRODUCT_KEY);
-	private static final String USER_KEY = "9LDpw47Ja0Lx0kfeXAOzdWoMMqEpWT3q";
+	private static final String USER_KEY = "iEANyI5Id9alYDNe2K6dBD6omsGWWKLY";
 	private static final String USER_PASS = HmacSHA1.getHmacSHA1(USER_KEY, format("clientId%sdeviceName%sproductKey%stimestamp%d", DEVICE_NAME, DEVICE_NAME, PRODUCT_KEY, timestamp));
 
-	private static final String TOPIC = "/" + PRODUCT_KEY + "/" + DEVICE_NAME + "/message";
+	private static final String TOPIC = "/" + PRODUCT_KEY + "/" + DEVICE_NAME + "/data";
 	
 
 	public static void main(String[] args) throws IOException {
-		TiWLAN.getInstance().startup(10000);
+		
+		TiWiFi.getInstance().startup(10000);
 		TiDNS.getInstance().startup();
 				
 		MqttConnectOptions connOpts = new MqttConnectOptions();
